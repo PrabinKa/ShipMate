5d807bcd10c3bc6ec812caa241b169daed988098
Prabin Karki <prabin.karki@amniltech.com>
2026-02-13 13:04:15 +0545

    project setup

---
+++ src/utils/index.ts
@@ -0,0 +1,10 @@
+import { getFontSize } from "./responsive/font";
+import { rWidth, rHeight, rSpacing } from "./responsive/layout";
+
+
+export {
+    getFontSize,
+    rWidth,
+    rHeight,
+    rSpacing
+}
